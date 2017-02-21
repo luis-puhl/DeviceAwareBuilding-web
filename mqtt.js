a34fa982bd27b0e428dccf238258adee7030560f
luis-puhl <luispuhl@gmail.com>
2017-02-21 10:30:46 -0300

    Testing with MQTT-WS: WS now receives all MQTT messages.

--- mqtt.js
+++ mqtt.js
@@ -11,24 +11,47 @@ clientMqtt.on('error', function (err) {
 })
 clientMqtt.on('connect', function () {
 	console.log('MQTT connect');
-	clientMqtt.subscribe('ADMIN');
-	clientMqtt.subscribe('devices');
+	clientMqtt.subscribe('dab/#');
 })
 clientMqtt.on('message', function (topic, message) {
 	// message is Buffer
-	console.log(message.toString());
-	switch (topic.toString()){
-		case 'devices/report':
-			// retrieve sensor report
-			break;
-		case 'ADMIN':
-			switch (message.toString()) {
-				case 'echo':
-					clientMqtt.publish(topic, 'ack');
-					break;
-				default:
-			}
-			break;
-		default:
+	let topicStr = topic.toString();
+	let messageStr = message.toString();
+
+	if (!sharedWS){
+		return;
 	}
-})
+	sharedWS.send( JSON.stringify({
+		mqtt: {
+			topic: topicStr,
+			message: messageStr,
+		},
+	}) );
+});
+
+console.log("mqtt client created");
+
+/* ------------------------------------------------------------------------ */
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({port: 80});
+
+let sharedWS = false;
+
+wss.on('connection', function connection(ws) {
+	console.info("websocket connection open");
+	ws.send('wellcome');
+	sharedWS = ws;
+
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+		ws.send(`echo: ${message}`);
+	});
+
+	ws.on("close", function () {
+		sharedWS = false;
+		console.log("websocket connection close");
+	});
+});
+
+console.log("websocket server created");

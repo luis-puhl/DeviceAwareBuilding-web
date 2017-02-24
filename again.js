2802b8d020c34a05fd2e536c744f755e2ced9403
luis-puhl <luispuhl@gmail.com>
2017-02-24 15:52:37 -0300

    Still express.static does not work =(

--- again.js
+++ again.js
@@ -1,6 +1,24 @@
-var express = require('express');
-var expressApp = express();
-var expressWs = require('express-ws')(expressApp);
+const http = require('http');
+const express = require('express');
+const expressWs = require('express-ws');
+
+const webRoot = './public';
+const webPort = 80;
+const webSocketsOptions = {
+	// You should not use autoAcceptConnections for production
+	// applications, as it defeats all standard cross-origin protection
+	// facilities built into the protocol and the browser.	You should
+	// *always* verify the connection's origin and decide whether or not
+	// to accept it.
+	autoAcceptConnections: false,
+};
+const ewsOptions = {
+	wsOptions: webSocketsOptions,
+};
+
+let httpServer = http.createServer();
+let expressApp = express();
+let ewsApp = expressWs(expressApp, httpServer, ewsOptions);
 
 expressApp.use(function (req, res, next) {
 	console.log('middleware');
@@ -8,21 +26,24 @@ expressApp.use(function (req, res, next) {
 	return next();
 });
 
-expressApp.use(express.static(__dirname + '/public'));
+expressApp.use(express.static(__dirname + webRoot));
 
 expressApp.ws('/', function(webSocketServer, req) {
 	webSocketServer.on('message', function incoming(message) {
-		console.log('received: %s', message);
+		console.log(`received: ${message}`);
 		webSocketServer.send(`echo: ${message}`);
 	});
-	webSocketServer.on("close", function () {
-		console.log("websocket connection close");
+	webSocketServer.on('close', function () {
+		console.log('websocket connection close');
 	});
 	webSocketServer.on('connection', function connection(ws) {
-		console.info("websocket connection open");
+		console.info('websocket connection open');
 		ws.send('wellcome');
 	});
-	console.log('socket', req.testing);
+	console.log(`socket req.testing=${req.testing}`);
 });
 
-expressApp.listen(80);
+expressApp.listen(webPort);
+httpServer.on('close', () => console.log(`httpServer now CLOSED`));
+httpServer.on('close', () => console.log(`httpServer now CLOSED`));
+httpServer.on('listening', () => console.log(`Now listen to ${httpServer.address()}:${webPort}`));
